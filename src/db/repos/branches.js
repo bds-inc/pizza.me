632eb4cf1d13a3682ff77ba896240bfc65df7c66
Jeremy Njoroge <jeremykent2726@gmail.com>
2016-09-28 19:04:43 +0000

    Added select_all query to src/db/repos/branches.js

--- src/db/repos/branches.js
+++ src/db/repos/branches.js
@@ -3,6 +3,9 @@ const sql  = require('../sql').branches
 module.exports = (rep, pgp) => {
   return {
     insert: (name, location, address, contact_info) =>
-      rep.one(sql.insert_one, name, location, address, contact_info, branch => branch.id)
+      rep.one(sql.insert_one, name, location, address, contact_info, branch => branch.id),
+
+    select: () => 
+      rep.any(sql.select_all)
   }
 }
