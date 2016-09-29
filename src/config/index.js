72c3dc821d4ca2809ae77e49948a581b50d79f7e
Jeremy Njoroge <jeremykent2726@gmail.com>
2016-09-29 05:12:34 +0000

    Added config files...

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  dev: {
+     host:'localhost',
+     port:5432,
+     database:'pizzame',
+     user:'pizzauser',
+     password:'bdsinc@koding'
+  },
+  prod: {
+     host:'localhost',
+     port:5432,
+     database:'pizzame',
+     user:'pizzauser',
+     password:'bdsinc@koding',
+     ssl:true
+  }
+}
