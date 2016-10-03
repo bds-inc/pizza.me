17e7ea32fdda9983b9adea53b1012823f55c5bdf
Jeremy Njoroge <jeremykent2726@gmail.com>
2016-10-03 05:30:49 +0000

    Added routes to branches - modified insert - added update - added find - modified '/' to include insert and post - added '/:branch_id' to include get, update and delete - added branch table operations for new routes to src/db/repos/branches.js

--- src/db/repos/branches.js
+++ src/db/repos/branches.js
@@ -2,10 +2,20 @@ const sql  = require('../sql').branches
 
 module.exports = (rep, pgp) => {
   return {
-    insert: (name, location, address, contact_info) =>
-      rep.one(sql.insert_one, name, location, address, contact_info, branch => branch.id),
+    # Adds a new branch and returns the new id:
+    insert: values =>
+      rep.one(sql.insert_one, values, branch => branch.id),
 
+    # Returns all branch records:
     all: () => 
-      rep.any(sql.select_all)
+      rep.any(sql.select_all),
+
+    update: values =>
+      rep.one(sql.update, values, branch => branch.id),
+
+    # Tries to find a branch by id:
+    find: id =>
+      rep.oneOrNone(sql.select_by_id, id)
+    
   }
 }
--- src/routes/branches.js
+++ src/routes/branches.js
@@ -22,6 +22,74 @@ router.get('/about', (req, res) =>
 )
 
 // Home page route for branches
-get('/', () => db.branches.all())
+router.route('/')
+  // Add a branch:
+  .post((req, res) => {
+    let body = req.body
+    db.branches.insert({
+      name         : body.name,
+      address      : body.address,
+      location     : body.location,
+      contact_info : body.contact_info
+    })
+      .then(
+        res.json({ success:true, data })
+      )
+      .catch(error => 
+        res.json({ success:false, error: error.message || error })
+      )
+  })
+  // Get all branches
+  .get((req, res) => {
+    db.branches.all(req)
+      .then(data =>
+        res.json({ success: true, data })
+      )
+      .catch(error =>
+        res.json({ success: true, data })
+      )
+  })
+
+// 
+router.route('/:branch_id')
+  // get branch info by ID:
+  .get((req, res) => {
+    db.branches.find(req.params.branch_id)
+      .then(data =>
+        res.json({ success:true, data })
+      )
+      .catch(error =>
+        res.json({ success:false, error: error.message || error })
+      )
+  })
+  // Update the branch given its ID:
+  .patch((req, res) => {
+    let body = req.body
+    db.branches.update({
+      name         : body.name,
+      address      : body.address,
+      location     : body.location,
+      contact_info : body.contact_info
+    })
+      .then(data =>
+        res.json({ success:true, data })
+      )
+      .catch(error =>
+        res.json({ success:false, error: error.message || error })
+      )
+  })
+  // Remove  branch by its ID:
+  .delete((req, res) => {
+    db.branches.remove(req.params.branch_id)
+      .then(data =>
+        res.json({ success:true, data })
+      )
+      .catch(error =>
+        res.json({ success:false, error: error.message || error })
+      )
+  })
+
+// find a branch by ID:
+get('/find/id=:id', req => db.branches.find(+req.params.id))
 
 module.exports = router
